ddb48a4f270439449daabc879c86e46709b921db
Brian McVeigh <bmwinfusion@gmail.com>
2014-01-19 12:41:42 -0500

    Added examples of using JComboBox in a JOP and JFrame

---
+++ JComboJFrameExample/src/JComboFrame.java
@@ -0,0 +1,63 @@
+// Written by Brian McVeigh
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class JComboFrame extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JLabel titleLabel;
+	private JTextField textField;
+	private JComboBox comboBox;
+	private JButton submitButton;
+	
+	public JComboFrame() {
+		
+		setResizable(false);
+		setSize(300, 200);
+		setLayout(new GridLayout(4, 1));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Initialize and add text label to JFrame window
+		titleLabel = new JLabel("JFrame combo box example");
+		add(titleLabel);
+		
+		// Initialize and add text field to JFrame window
+		textField = new JTextField(10);
+		add(textField);
+		
+		// Create our array of combo box choices
+		String[] comboBoxChoices = {
+				"Select an option...",
+				"Choice 1", 
+				"Choice 2", 
+				"Choice 3"
+		};
+		
+		// Initialize and add combo box to JFrame window
+		comboBox = new JComboBox(comboBoxChoices);
+		add(comboBox);
+		
+	   // Initialize and add submit button to JFrame window. Add an ActionListener for the button
+		submitButton = new JButton("Submit");
+		ButtonListener bListener = new ButtonListener();
+		submitButton.addActionListener(bListener);
+		add(submitButton);
+	}
+	
+	private class ButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(null, "Info submitted\nText entered in text box: " + textField.getText() + 
+					"\nSelected option: " + comboBox.getSelectedItem());
+		}
+
+	}
+
+	
+}
---
+++ JComboJFrameExample/src/JComboJFrameExample.java
@@ -0,0 +1,11 @@
+// Written by Brian McVeigh
+
+public class JComboJFrameExample {
+
+	public static void main(String[] args) {
+		JComboFrame frame = new JComboFrame();
+		frame.setVisible(true);
+
+	}
+
+}

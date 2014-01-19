713ea10733f134408c9b96cd9d9cd1d3a1376799
Brian McVeigh <bmwinfusion@gmail.com>
2014-01-19 10:24:46 -0500

    first commit

---
+++ Documents/Xcode Projects/Checking Checkpoint/Checking Checkpoint/BMAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  BMAppDelegate.m
+//  Checking Checkpoint
+//
+//  Created by Brian McVeigh on 8/28/13.
+//  Copyright (c) 2013 Brian McVeigh. All rights reserved.
+//
+
+#import "BMAppDelegate.h"
+
+@implementation BMAppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end

c8cbc8cdcafc2c6b9138974cd05e03e5343e5d5b
bmcveigh <bb@bbs-imac.home>
2014-07-24 11:16:44 -0400

    Polished HTMLTableGenerator

---
+++ HTMLTableGenerator/src/HTMLTableGenerator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author brianmcveigh
+ */
+public class HTMLTableGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainWindow w = new MainWindow();
+        w.setVisible(true);
+    }
+}
